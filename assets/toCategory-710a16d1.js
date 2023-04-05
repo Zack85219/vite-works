fcc4dd400f57973379aef2647e890a81ce66154b
Zack85219 <j33241796@gmail.com>
2023-04-05 18:17:35 +0800

    deploy_final_work_test-3

--- assets/toCategory-710a16d1.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-4118f9dd.js";const n=e("category",{state:()=>({continent:""}),actions:{getCategory(t=""){this.continent=t,console.log(this.continent)}},getters:{}});export{n as c};
