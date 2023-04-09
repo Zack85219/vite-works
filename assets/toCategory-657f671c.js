574a12aab7c4351bf20a3af3deca27f7d636da74
Zack85219 <j33241796@gmail.com>
2023-04-09 21:53:46 +0800

    deploy_final_work_add_tooltips_bg

---
+++ assets/toCategory-657f671c.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-56446f1c.js";const n=e("category",{state:()=>({continent:""}),actions:{getCategory(t=""){this.continent=t,console.log(this.continent)}},getters:{}});export{n as c};
--- assets/toCategory-8262eba0.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-d04a7f04.js";const n=e("category",{state:()=>({continent:""}),actions:{getCategory(t=""){this.continent=t,console.log(this.continent)}},getters:{}});export{n as c};
